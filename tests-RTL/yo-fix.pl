7d0ac43caa4208a6534cc3265f653cd95f6afef2
Kevin Cameron <kc@ieee.org>
2025-11-30 15:13:37 -0800

    Update

--- tests-RTL/yo-fix.pl
+++ tests-RTL/yo-fix.pl
@@ -9,12 +9,25 @@ $trans{DFF}       = "dff";
 $trans{DFF_PP0}   = "dff";
 $trans{DFFE_PP0P} = "dffe";
 
+$primitive{AND}   = 1;
+$primitive{OR}    = 1;
+$primitive{NAND}  = 1;
+$primitive{NOR}   = 1;
+$primitive{NOT}   = 1;
+
 while (<>) {
     if (/\\\$_(\S*)_(\s.*)/) {
 	if ($g = $trans{$1}) {
 	    $_ = "  ".$g.$2;
-	    $need{$1} = 1;
+	    if (! $primitive{$1}) {
+		$need{$1} = 1;
+	    }
 	}  	 
     }
     print;
+}
+
+foreach $mod (keys %need) {
+    
+    print "`include \"../$mod.v\"\n";
 }
