3acc13ea5de1d354e2e91e7f9c27360ea895e1fe
Kevin Cameron <kc@ieee.org>
2025-12-01 14:21:44 -0800

    fixed

--- tests-atpg/shift_reg.v
+++ tests-atpg/shift_reg.v
@@ -2,10 +2,6 @@
 // Implements q_out = {q_in[2:0], si}
 module shift_reg (
     input si,
-    input q_in0,
-    input q_in1,
-    input q_in2,
-    input q_in3,
     output q_out0,
     output q_out1,
     output q_out2,
@@ -14,8 +10,8 @@ module shift_reg (
 
 // Shift left operation
 buf buf0 (q_out0, si);
-buf buf1 (q_out1, q_in0);
-buf buf2 (q_out2, q_in1);
-buf buf3 (q_out3, q_in2);
+buf buf1 (q_out1, q_out0);
+buf buf2 (q_out2, q_out1);
+buf buf3 (q_out3, q_out2);
 
 endmodule
